1d7cbaaf9df90ca2e144f1d07d657deb03c29772
suragch <suragch@suragch.dev>
2021-10-20 12:38:12 +0800

    updates on ring buffer'

--- lib/ring_buffer.dart
+++ lib/ring_buffer.dart
@@ -12,9 +12,15 @@ class RingBuffer<E> {
   bool get isEmpty => _length == 0;
   bool get isFull => _length == _list.length;
 
+  // return true if the write was successful.
+  // returns false if the buffer is full
   bool write(E? value) {
+    if (isFull) {
+      return false;
+    }
     _list[_writeIndex] = value;
     _moveWriteIndex();
+    _length++;
     return true;
   }
 
@@ -23,6 +29,24 @@ class RingBuffer<E> {
       _writeIndex = 0;
     } else {
       _writeIndex = _writeIndex + 1;
+    }
+  }
+
+  E? read() {
+    if (isEmpty) {
+      return null;
+    }
+    final value = _list[_readIndex];
+    _length = _length - 1;
+    _moveReadIndex();
+    return value;
+  }
+
+  void _moveReadIndex() {
+    if (_readIndex == _list.length - 1) {
+      _readIndex = 0;
+    } else {
+      _readIndex++;
     }
   }
 
---
+++ test/ring_buffer_test.dart
@@ -0,0 +1,50 @@
+import 'package:alg_class/ring_buffer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('filling up the ring buffer', () {
+    final ringBuffer = RingBuffer<int>(4);
+    expect(ringBuffer.isEmpty, true);
+    expect(ringBuffer.write(0), true);
+    expect(ringBuffer.write(0), true);
+    expect(ringBuffer.write(0), true);
+    expect(ringBuffer.write(0), true);
+    expect(ringBuffer.isFull, true);
+  });
+
+  test('overfull buffer', () {
+    final ringBuffer = RingBuffer<int>(4);
+    expect(ringBuffer.isEmpty, true);
+    expect(ringBuffer.write(0), true);
+    expect(ringBuffer.write(0), true);
+    expect(ringBuffer.write(0), true);
+    expect(ringBuffer.write(0), true);
+    expect(ringBuffer.isFull, true);
+
+    expect(ringBuffer.write(0), false);
+  });
+
+  test('reading a value from the buffer', () {
+    final ringBuffer = RingBuffer<int>(4);
+    var value = ringBuffer.read();
+    expect(value, null);
+
+    ringBuffer.write(22);
+    value = ringBuffer.read();
+    expect(value, 22);
+
+    ringBuffer.write(10);
+    ringBuffer.write(11);
+    ringBuffer.write(12);
+    ringBuffer.write(13);
+
+    value = ringBuffer.read();
+    expect(value, 10);
+    value = ringBuffer.read();
+    expect(value, 11);
+    value = ringBuffer.read();
+    expect(value, 12);
+    value = ringBuffer.read();
+    expect(value, 13);
+  });
+}

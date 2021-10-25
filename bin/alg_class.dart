36884dfa86b47b9dc4d07e381d9e362090806102
suragch <suragch@suragch.dev>
2021-10-25 10:47:51 +0800

    toString

--- bin/alg_class.dart
+++ bin/alg_class.dart
@@ -1,30 +1,13 @@
+import 'package:alg_class/binary_tree.dart';
 import 'package:alg_class/tree_node.dart';
 
 void main() {
-  final tree = createTree();
-  tree.traverseTree();
-}
-
-TreeNode<String> createTree() {
-  final drink = TreeNode('drink');
-  final hot = TreeNode('hot');
-  final cold = TreeNode('cold');
-  final coffee = TreeNode('coffee');
-  final tea = TreeNode('tea');
-  final soda = TreeNode('soda');
-  final juice = TreeNode('juice');
-  final cola = TreeNode('cola');
-  final sprite = TreeNode('sprite');
-  final pepsi = TreeNode('pepsi');
-  drink.addChild(hot);
-  drink.addChild(cold);
-  hot.addChild(coffee);
-  hot.addChild(tea);
-  cold.addChild(soda);
-  cold.addChild(juice);
-  soda.addChild(cola);
-  soda.addChild(sprite);
-  soda.addChild(pepsi);
-
-  return drink;
+  final tree = BinaryTreeNode('A',
+      leftChild: BinaryTreeNode('D'),
+      rightChild: BinaryTreeNode(
+        'R',
+        leftChild: BinaryTreeNode('T'),
+        rightChild: BinaryTreeNode('Fun'),
+      ));
+  print(tree);
 }
--- lib/binary_tree.dart
+++ lib/binary_tree.dart
@@ -3,4 +3,45 @@ class BinaryTreeNode<T> {
   T value;
   BinaryTreeNode? leftChild;
   BinaryTreeNode? rightChild;
+
+  @override
+  String toString() {
+    return _toString(StringBuffer(), true, StringBuffer()).toString();
+  }
+
+  StringBuffer _toString(StringBuffer prifix, bool isTail, StringBuffer sb) {
+    if (rightChild != null) {
+      final newBuffer = StringBuffer();
+      newBuffer.write(prifix);
+      newBuffer.write(isTail ? '│   ' : '    ');
+      rightChild!._toString(newBuffer, false, sb);
+    }
+    sb.write(prifix);
+    sb.write(isTail ? '└── ' : '┌── ');
+    sb.write(value.toString());
+    sb.write('\n');
+    if (leftChild != null) {
+      final newBuffer = StringBuffer();
+      newBuffer.write(prifix);
+      newBuffer.write(isTail ? '    ' : '│   ');
+      leftChild!._toString(prifix, true, sb);
+    }
+    return sb;
+  }
 }
+
+// public StringBuilder toString(StringBuilder prefix, boolean isTail, StringBuilder sb) {
+//     if(right!=null) {
+//         right.toString(new StringBuilder().append(prefix).append(isTail ? "│   " : "    "), false, sb);
+//     }
+//     sb.append(prefix).append(isTail ? "└── " : "┌── ").append(value.toString()).append("\n");
+//     if(left!=null) {
+//         left.toString(new StringBuilder().append(prefix).append(isTail ? "    " : "│   "), true, sb);
+//     }
+//     return sb;
+// }
+
+// @Override
+// public String toString() {
+//     return this.toString(new StringBuilder(), true, new StringBuilder()).toString();
+// }

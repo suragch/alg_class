d1c70aab5ee98341c001e6fb3f7b77f86e8e6893
suragch <suragch@suragch.dev>
2021-11-08 11:27:07 +0800

    binary search

--- bin/alg_class.dart
+++ bin/alg_class.dart
@@ -1,30 +1,22 @@
-import 'package:alg_class/binary_search_tree.dart';
-
 void main() {
-  final tree = BinarySearchTree<int>();
-  tree.insert(43);
-  tree.insert(10);
-  tree.insert(79);
-  tree.insert(90);
-  tree.insert(12);
-  tree.insert(54);
-  tree.insert(11);
-  tree.insert(9);
-  tree.insert(50);
-
-  print(tree);
-  final isInTree = tree.contains(66);
-  print(isInTree);
-  print(tree);
-
-  final people = ['Bob', 'Mary', 'Susan'];
-  greetEveryone(people);
-  greetEveryone(people);
+  final list = [0, 10, 11, 15, 30, 35, 60, 71, 80];
+  final isInList = binarySearch(61, list);
+  print(isInList);
 }
 
-void greetEveryone(List<String> people) {
-  for (final person in people) {
-    print('Welcome, $person!');
+bool binarySearch(int value, List<int> list) {
+  var start = 0;
+  var end = list.length;
+
+  while (start != end) {
+    var middle = (start + end) ~/ 2;
+    if (list[middle] == value) {
+      return true;
+    } else if (value > list[middle]) {
+      start = middle + 1;
+    } else {
+      end = middle;
+    }
   }
-  people[0] = 'Hacker';
+  return false;
 }

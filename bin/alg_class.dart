c36f3973f7c4c54ae00ab8ea3a0c59b6987d03a9
suragch <suragch@suragch.dev>
2021-11-11 13:06:50 +0800

    notes for binary tree

--- bin/alg_class.dart
+++ bin/alg_class.dart
@@ -1,22 +1,44 @@
 void main() {
-  final list = [0, 10, 11, 15, 30, 35, 60, 71, 80];
-  final isInList = binarySearch(61, list);
-  print(isInList);
+  final tree = BinaryTreeNode(
+    1,
+    leftChild: BinaryTreeNode(2),
+    rightChild: BinaryTreeNode(3),
+  );
+  print(tree);
+  tree.traverseInOrder(print);
+  // tree.check(1);
+  // tree.traverseInOrder((value) {
+  //   print(value);
+  // });
 }
 
-bool binarySearch(int value, List<int> list) {
-  var start = 0;
-  var end = list.length;
+class BinaryTreeNode<T> {
+  BinaryTreeNode(this.value, {this.leftChild, this.rightChild});
+  T value;
+  BinaryTreeNode<T>? leftChild;
+  BinaryTreeNode<T>? rightChild;
 
-  while (start != end) {
-    var middle = (start + end) ~/ 2;
-    if (list[middle] == value) {
-      return true;
-    } else if (value > list[middle]) {
-      start = middle + 1;
-    } else {
-      end = middle;
-    }
+  void traverseInOrder(void Function(T value) traverse) {
+    leftChild?.traverseInOrder(traverse);
+    traverse(value);
+    rightChild?.traverseInOrder(traverse);
   }
-  return false;
+
+  void check(T value) {
+    traverseInOrder((myValue) {
+      if (value == myValue) {
+        print(myValue);
+      }
+    });
+  }
+
+  @override
+  String toString() {
+    var buffer = StringBuffer();
+    traverseInOrder((value) {
+      buffer.write(value.toString());
+      buffer.write(' ');
+    });
+    return buffer.toString();
+  }
 }

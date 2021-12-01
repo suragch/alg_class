7586ec1143764b20c328d43340dbc1336a0d8a4b
suragch <suragch@suragch.dev>
2021-12-01 14:19:32 +0800

    merge sort partial

--- bin/alg_class.dart
+++ bin/alg_class.dart
@@ -1,9 +1,10 @@
 import 'package:alg_class/bubble_sort.dart';
 import 'package:alg_class/insertion_sort.dart';
+import 'package:alg_class/merge_sort.dart';
 
 void main() {
   final list = [4, 3, 1, 4, 6, 7, 8];
   print(list);
-  list.sort();
-  print(list);
+  final sorted = mergeSort(list);
+  print(sorted);
 }
